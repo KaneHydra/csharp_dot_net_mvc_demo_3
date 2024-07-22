5407971dc5a817aea67e2461148246f4c371b363
KaneHydra <kanehydra@gmail.com>
2024-07-22 14:07:18 +0800

    加入簡單的登入權限控管功能

--- LabMVC3/Controllers/HomeController.cs
+++ LabMVC3/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 using LabMVC3.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace LabMVC3.Controllers
 {
+    // 驗證是否登入，如果沒有登入，則redirect回到登入頁面
+    // 如果發現有缺少using,在紅色波浪處按Ctrl+.
+    // 他會跳出可能需要using的lib
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
@@ -13,6 +18,8 @@ namespace LabMVC3.Controllers
             _logger = logger;
         }
 
+        // 這個頁面允許匿名，不用登入就能檢視
+        [AllowAnonymous]
         public IActionResult Index()
         {
             return View();
